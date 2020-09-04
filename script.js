9495d48d6f6bdbfb925e2190ce4919540e16f74a
Torion <teezus318@gmail.com>
2020-09-03 20:21:22 -0500

    Studio 16 Completed

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// TODO: add code here
+window.addEventListener('load', function(){
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json')
+    .then(response => response.json())
+    .then(data => {
+        let astronauts = data;
+        let listOfAstronauts = '';
+        let div = document.getElementById('container');
+        astronauts.forEach(astronaut => {
+            listOfAstronauts +=`
+            <div class="astronaut">
+                <div class="bio">
+                    <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                        <li>Active: ${astronaut.active}</li>
+                        <li>Skills: ${astronaut.skills}</li>
+                    </ul>
+                </div>
+                <img class="avatar" src="${astronaut.picture}">
+            </div>`
+        });
+        div.innerHTML = listOfAstronauts;
+    });
+})
