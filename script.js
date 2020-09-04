80e4e2f818c7a5d868208c757f2fa138c6521d66
Torion <teezus318@gmail.com>
2020-09-03 20:57:24 -0500

    Studio 16 done with bonuses

--- script.js
+++ script.js
@@ -5,20 +5,22 @@ window.addEventListener('load', function(){
         let astronauts = data;
         let listOfAstronauts = '';
         let div = document.getElementById('container');
+        astronauts.sort((a,b) => (a.hoursInSpace < b.hoursInSpace) ? 1 : -1)
         astronauts.forEach(astronaut => {
+            let active = `${astronaut.active ? `<li><span>Active: ${astronaut.active}</span></li>` : `<li>Active: ${astronaut.active}</li>`}`
             listOfAstronauts +=`
             <div class="astronaut">
                 <div class="bio">
                     <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
                     <ul>
                         <li>Hours in space: ${astronaut.hoursInSpace}</li>
-                        <li>Active: ${astronaut.active}</li>
+                        ${active}
                         <li>Skills: ${astronaut.skills}</li>
                     </ul>
                 </div>
                 <img class="avatar" src="${astronaut.picture}">
             </div>`
         });
-        div.innerHTML = listOfAstronauts;
+        div.innerHTML = listOfAstronauts + `<p>There's ${astronauts.length} astronauts showing currently</p>`;
     });
 })
